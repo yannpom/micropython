e340c271c2677f7019f3c07ad66276fbe758e65d
Yann Pomarède <yann.pomarede@gmail.com>
2020-01-07 23:41:40 +0100

    websocket

--- ports/esp32/modules/servostep.py
+++ ports/esp32/modules/servostep.py
@@ -1,8 +1,10 @@
 
 import _servostep as ps
 import time
-
-
+import socket
+import uwebsocket
+import websocket_helper
+import _thread
 
 from math import pi
 DEGREE = pi/180.0
@@ -15,10 +17,70 @@ class Servostep:
     interactiveMode = True
 
 
+    data_socket_listen = None
+    data_socket_client = None
+    data_websocket = None
+
+
+    def __init__(self):
+        self._start_data_websocket()
+        _thread.start_new_thread(self._thread_send_current_status_to_ws, ())
+
+
     def run(self):
         ps.run()
         self._wait_completion()
-        ps.power_off()
+        #ps.power_off()
+
+
+    def _thread_send_current_status_to_ws(self):
+        while True:
+            try:
+                if self.data_websocket:
+                    self.data_websocket.write('{"p": %f}' % ps.get_position())
+                else:
+                    print("No WS to send data")
+                time.sleep(0.01)
+            except OSError:
+                print("WS error during write")
+            except KeyboardInterrupt:
+                print("KeyboardInterrupt ignored from thread")
+
+
+
+    def _start_data_websocket(self):
+        PORT = 8080
+        print("Websocket on port %d" % PORT)
+        self.data_socket_listen = socket.socket()
+        self.data_socket_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        ai = socket.getaddrinfo("0.0.0.0", PORT)
+        addr = ai[0][4]
+
+        self.data_socket_listen.bind(addr)
+        self.data_socket_listen.listen(1)
+        self.data_socket_listen.setsockopt(socket.SOL_SOCKET, 20, self._accept_conn)
+
+    def _accept_conn(self, socket):
+        print("accept_conn")
+        client, remote_addr = socket.accept()
+        print("\nConnection from:", remote_addr)
+
+        if self.data_websocket:
+            print("Closing previous WS")
+            self.data_websocket.close()
+            self.data_websocket = None
+
+        if self.data_socket_client:
+            print("Closing previous socket")
+            self.data_socket_client.close()
+            self.data_socket_client = None
+
+        self.data_socket_client = client
+        websocket_helper.server_handshake(client)
+        self.data_websocket = uwebsocket.websocket(self.data_socket_client, True)
+        self.data_socket_client.setblocking(False)
+
 
     def _push_cmd(self, cmd):
         print("Pushing %s" % cmd)
@@ -31,27 +93,29 @@ class Servostep:
             p=cmd.p,
             v=cmd.v,
             t=cmd.t,
-            end_prop=cmd.end.prop if cmd.end else None,
+            end_prop=cmd.end.prop if cmd.end else 0,
             end_value=cmd.end.value if cmd.end else None,
-            end_comp=cmd.end.comp if cmd.end else None,
+            end_comp=cmd.end.comp if cmd.end else 0,
             )
 
         if self.interactiveMode:
-            ps.run()
-            self._wait_completion()
-            ps.power_off()
-
+            self.run()
 
     def _wait_completion(self):
         print("Waiting on queue")
         try:
             while ps.is_running():
-                time.sleep(0.01)
+                time.sleep(0.1)
         except KeyboardInterrupt:
             ps.power_off()
             raise KeyboardInterrupt
         print("Done")
 
+    def log_reset(self):
+        ps.push_log_reset()
+
+    def log_start(self, length=800):
+        ps.push_log_start(length)
 
     def setPosition(self,
                     position=None,
@@ -61,6 +125,7 @@ class Servostep:
                     until_absolute_error_greater_than=None,
                     until_error_lower_than=None,
                     until_error_greater_than=None,
+                    until_timeout=None,
                     ):
 
         end = None
@@ -72,8 +137,11 @@ class Servostep:
             end = EndCondition(ps.END_PROP_POSITION_ERROR, until_error_lower_than, ps.END_COMP_LT)
         elif until_error_greater_than is not None:
             end = EndCondition(ps.END_PROP_POSITION_ERROR, until_error_greater_than, ps.END_COMP_GT)
+        elif until_timeout is not None:
+            end = EndCondition(ps.END_PROP_TIME, until_timeout, 0)
+            
 
-        mode = ps.MODE_POSITION_ABS if position else ps.MODE_POSITION_REL
+        mode = ps.MODE_POSITION_ABS if position is not None else ps.MODE_POSITION_REL
 
         if position is None: position = 0.0
 
@@ -84,6 +152,7 @@ class Servostep:
     def setSpeed(self,
                  velocity,
                  max_torque=1.0,
+                 #max_acceleration=10.0,
                  until_position_lower_than=None,
                  until_position_greater_than=None,):
         end = None
@@ -137,54 +206,27 @@ class EndCondition:
 
 if __name__ == "__main__":
 
-    # s1 = Scenario([
-    #         Command('p', 5, end=True),
-    #         Command('v', 50, end=EndCondition("position", 5, ">=")),
-    #      ])
-
-    # s1.loop = 2
-
-    # m = Servostep()
-    # m.run(s1)
-
-    # m.run(Scenario([Command('v', p=0, v=10, end=EndCondition("time", 0.1)),]))
-
-
-
-    from servostep import *
-    m = Servostep()
-    m.setPosition(300*DEGREE, until_absolute_error_lower_than=0.5*DEGREE)
     
 
 
-
+    from servostep import *
+    import _servostep as ps
     m = Servostep()
+    m.interactiveMode=False
+    ps.set_origin((-417+1)*DEGREE)
+    m.setPosition(0, until_absolute_error_lower_than=0.5*DEGREE)
+    m.setPosition(0, max_torque=0.20, until_error_greater_than=2*DEGREE)
+    m.setPosition(0, max_torque=0.20, until_timeout=0.3)
+    m.setPosition(20*DEGREE, max_torque=0.20, max_velocity=0.2*RPS, until_absolute_error_lower_than=0.5*DEGREE)
+    m.setSpeed(-5*RPS, until_position_lower_than=9*DEGREE)
+    m.setPosition(10*DEGREE, until_timeout=0.1)
     while True:
-
-        m.interactiveMode = True
-        #m.setScenarioMode()
-
-        # Go to idle position and wait it is reached within 0.5 degree
-        m.setPosition(300*DEGREE, until_absolute_error_lower_than=0.5*DEGREE)
-        
-        # Lower the torque to 5% and wait for a -2 degrees disturbance
-        m.setPosition(max_torque=0.05, until_error_lower_than=-2*DEGREE)
-        
-        # An object moved the rotor by more than 2 degrees, starting the catapult cycle!
-
-        # Set a high negative speed until a -30 degree position
-        m.setSpeed(-1200*RPM, until_position_lower_than=270*DEGREE)
-
-        # Set a high positive speed until a +15 degree position
-        m.setSpeed(1200*RPM, until_position_greater_than=315*DEGREE)
-
-        # Go back to idle position
-        m.setPosition(300*DEGREE, until_absolute_error_lower_than=0.5*DEGREE)
-
-        m.runScenario()
+        m.run()
 
 
 
 
-
+    ps.getOrigin()
+    ps.setOrigin() # sets with current position
+    ps.setOrigin(-7.29)
 
